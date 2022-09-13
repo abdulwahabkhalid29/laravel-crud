b49282f3b6f1a8d94d545ed09bf2318d86c63095
abdulwahabkhalid <98513650+MFahadKhalid@users.noreply.github.com>
2022-09-14 12:16:11 +1300

    laravel-crud

--- database/migrations/2022_08_29_051156_create_factories_table.php
+++ database/migrations/2022_08_29_051156_create_factories_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('address');
-            $table->integer('phone');
+            $table->string('phone');
             $table->timestamps();
         });
     }
--- database/migrations/2022_08_29_070122_create_workers_table.php
+++ database/migrations/2022_08_29_070122_create_workers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('workers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('email');
             $table->string('factory_id');
             $table->string('country_id');
